6b8a42dec8cd14c8845d13368bb3aeede8562c91
shishe84 <shishe@gmail.com>
2017-07-11 18:09:07 -0300

    Checkbox alert

--- src/pages/alerts/alerts.ts
+++ src/pages/alerts/alerts.ts
@@ -134,4 +134,80 @@ export class AlertsPage {
     });
   }
 
+  testCheckboxOpen: boolean;
+  testCheckboxResult;
+
+  showCheckbox() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Which planets have you visited?');
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Alderaan',
+      value: 'value1',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Bespin',
+      value: 'value2'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Coruscant',
+      value: 'value3'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Endor',
+      value: 'value4'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Hoth',
+      value: 'value5'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Jakku',
+      value: 'value6'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Naboo',
+      value: 'value7'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Takodana',
+      value: 'value8'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Tatooine',
+      value: 'value9'
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Okay',
+      handler: data => {
+        console.log('Checkbox data:', data);
+        this.testCheckboxOpen = false;
+        this.testCheckboxResult = data;
+      }
+    });
+    alert.present().then(() => {
+      this.testCheckboxOpen = true;
+    });
+  }
+
 }
