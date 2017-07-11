c0e8a41b4d61a2f5ba6243897b91a6c9a7a50997
shishe84 <shishe@gmail.com>
2017-07-11 16:01:00 -0300

    Radio alert

--- src/pages/alerts/alerts.ts
+++ src/pages/alerts/alerts.ts
@@ -47,4 +47,91 @@ export class AlertsPage {
     prompt.present();
   }
 
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Use this lightsaber?',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  testRadioOpen: boolean;
+  testRadioResult;
+
+  showRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Lightsaber color');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Blue',
+      value: 'blue',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Green',
+      value: 'green'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Red',
+      value: 'red'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Yellow',
+      value: 'yellow'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Purple',
+      value: 'purple'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'White',
+      value: 'white'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Black',
+      value: 'black'
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Ok',
+      handler: data => {
+        console.log('Radio data:', data);
+        this.testRadioOpen = false;
+        this.testRadioResult = data;
+      }
+    });
+
+    alert.present().then(() => {
+      this.testRadioOpen = true;
+    });
+  }
+
 }
